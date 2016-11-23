8f24174b6b3ab8612202a460fbffa29d3da3cd55
勾承甫 <hhzgouchengfu@corp.netease.com>
2016-11-23 20:24:36 +0800

    no message

---
+++ BugEase_iOS.framework/Headers/BEManager.h
@@ -0,0 +1,57 @@
+//
+//  BEManager.h
+//  BugEase_iOS
+//
+//  Created by jc on 16/8/1.
+//  Copyright © 2016年 jc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+typedef NS_ENUM(NSInteger, BEModel){
+    BEInvocationEventNone, // 0 直接启动
+    BEInvocationEventShake, // 1 摇一摇启动
+    BEInvocationEventFloatButton, // 2 悬浮窗启动
+    BEInvocationEventScreenShot, // 3 截屏启动
+    BEInvocationEventTwoFingerSwipe // 4 双指右滑启动
+};
+
+@interface BEManager : NSObject
++(id)shareManagerWithAppKeys:(NSString *) appKey withModel:(NSUInteger) model;
+
+//用户账号信息接口
++(void) setUserAccountInfo:(NSString*) userAccountInfo;
+
+//取消用户账号信息
+    
++(void) clearUserAccountInfo;
+
+//设置系统日志大小
+
++(void) setSystemLogSizeWithKB:(NSInteger) sizeWithKB;
+
+//设置应用日志路径
+
++(void) setApplyLogPath:(NSString*) logPath;
+//设置应用日志大小
++(void) setApplyLogSizeWithKB:(NSInteger) sizeWithKB;
+
+//设置操作步数
+
++(void) setUserOperateMaxNum:(NSInteger) maxNum;
+
+
+//设置自定义字段
+
++(void) setUserData:(NSString*) key andValue:(NSString*) value;
+
+//自己直接调用API上传文件
+
++(void) sendFeedBackWithType:(NSUInteger)type andContent:(NSString*) feedContent andReporter:(NSString*)reporter andImagePath:(NSString*)imagePath;
+
+
+//Demo app 专用API
+
++(void) selectModel:(BEModel)model;
+
+@end
---
+++ BugEase_iOS.framework/Headers/BugEase_iOS.h
@@ -0,0 +1,19 @@
+//
+//  BugEase_iOS.h
+//  BugEase_iOS
+//
+//  Created by jc on 16/8/1.
+//  Copyright © 2016年 jc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "BEManager.h"
+//! Project version number for BugEase_iOS.
+FOUNDATION_EXPORT double BugEase_iOSVersionNumber;
+
+//! Project version string for BugEase_iOS.
+FOUNDATION_EXPORT const unsigned char BugEase_iOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BugEase_iOS/PublicHeader.h>
+
+
